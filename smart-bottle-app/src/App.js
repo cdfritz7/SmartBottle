50c92bdde52d184df995d399b625647ca08a2d32
coreykarnei <coreykarnei@utexas.edu>
2020-11-30 12:02:28 -0600

    removed an S

--- smart-bottle-app/src/App.js
+++ smart-bottle-app/src/App.js
@@ -133,9 +133,9 @@ class App extends Component{
           </div>
 
           <div className="my-container">
-            <p className="sub-heading"> Conclusions </p>
+            <p className="sub-heading"> Conclusion </p>
             <p className="txt">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              Lorem dolor sit amet, consectetur adipiscing elit,
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
